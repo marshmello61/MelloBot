aca55304da5f2221587262f83a3e376650b65e3c
Mayur Varde <ultramayur123@gmail.com>
2020-06-04 22:29:13 +0530

    Fix .carbon6 indentation
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -381,7 +381,7 @@ async def carbon_api(e):
     # Removing carbon.png after uploading
     await e.delete()  # Deleting msg
  
- @register(outgoing=True, pattern="^.carbon6")
+@register(outgoing=True, pattern="^.carbon6")
 async def carbon_api(e):
     """ A Wrapper for carbon.now.sh """
     await e.edit("`Processing..`")

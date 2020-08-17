427f83ec521da4e4e3b46b08a24f49f756335ef4
Mayur <ultramayur123@gmail.com>
2020-08-17 13:07:02 +0530

    increase kang autodelete time
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -168,8 +168,8 @@ async def kang(event):
         f"{emoji} as the emoji! "
         f"This pack can be found `[here](t.me/addstickers/{packname})",
         parse_mode='md')
-    await asyncio.sleep(7.5)
-    await event.delete()    
+    await asyncio.sleep(11)
+    await event.delete()
 
 
 async def newpack(is_anim, sticker, emoji, packtitle, packname):

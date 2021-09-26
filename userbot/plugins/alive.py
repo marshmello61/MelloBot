30508dd9fa258364726269b871f3a27589bb11cb
Mayur <ultramayur123@gmail.com>
2021-09-26 05:42:30 +0000

    4
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -44,7 +44,7 @@ async def amireallyalive(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
-    EMOJI = gvarstatus("ALIVE_EMOJI") or "  ✥ "
+    EMOJI = gvarstatus("ALIVE_EMOJI") or "🔺"
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "**// Project MelloBot running fine af //**"
     CAT_IMG = gvarstatus("ALIVE_PIC")
     cat_caption = gvarstatus("ALIVE_TEMPLATE") or temp
